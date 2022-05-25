0f930fbfde2f929d7f28af78aa429072c03cc6ba
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-05-25 21:03:00 +0300

    add domain part(entity layer(persistent), repository)

--- src/main/java/com/realo/estate/RealEstateAppApplication.java
+++ src/main/java/com/realo/estate/RealEstateAppApplication.java
@@ -1,11 +1,9 @@
 package com.realo.estate;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class RealEstateAppApplication {
 
     public static void main(String[] args) {
