60c72f88a4605432d8b0451758a8203d3777ddb6
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-06-19 09:25:14 +0300

    update Role (add Spring Security)

--- src/main/java/com/realo/estate/domain/persistence/user/Role.java
+++ src/main/java/com/realo/estate/domain/persistence/user/Role.java
@@ -3,13 +3,14 @@ package com.realo.estate.domain.persistence.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Set;
 
 @Getter
 @AllArgsConstructor
 @ToString
-public enum Role {
+public enum Role implements GrantedAuthority {
 
     USER(Set.of(Permission.ANNOUNCEMENT_READ, Permission.ANNOUNCEMENT_WRITE,
             Permission.ESTATE_READ, Permission.ESTATE_WRITE, Permission.PROVIDER_READ)),
@@ -22,4 +23,9 @@ public enum Role {
             Permission.PROVIDER_READ, Permission.PROVIDER_WRITE));
 
     private final Set<Permission> permissions;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
