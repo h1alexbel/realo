91d1ba5886de8f6405a9eb95d94f1fcfa5702a3f
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-06-22 16:17:17 +0300

    add AuthenticationUserService

---
+++ src/main/java/com/realo/estate/web/security/AuthenticationUserService.java
@@ -0,0 +1,7 @@
+package com.realo.estate.web.security;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AuthenticationUserService extends UserDetailsService {
+}
