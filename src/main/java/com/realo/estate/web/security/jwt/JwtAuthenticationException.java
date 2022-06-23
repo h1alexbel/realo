8c0e839b9d6b5d556160dcffd88be4c50b9b0ca6
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-06-23 13:59:40 +0300

    Jwt Security added

---
+++ src/main/java/com/realo/estate/web/security/jwt/JwtAuthenticationException.java
@@ -0,0 +1,21 @@
+package com.realo.estate.web.security.jwt;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+
+@Getter
+public class JwtAuthenticationException extends AuthenticationException {
+
+    private final HttpStatus httpStatus;
+
+    public JwtAuthenticationException(String msg, HttpStatus httpStatus) {
+        super(msg);
+        this.httpStatus = httpStatus;
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+        httpStatus = null;
+    }
+}
